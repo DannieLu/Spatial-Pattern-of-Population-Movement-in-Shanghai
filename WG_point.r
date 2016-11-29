85639c391d5eb5e2e2c0bf4fce9ef5dc1d605478
Tina Gao <wenyu6@vt.edu>
2016-11-29 18:26:13 -0500

    update intermediate

--- WG_point.r
+++ WG_point.r
@@ -4,135 +4,181 @@ library(akima)
 library(geoR)
 library(xtable)
 library(MASS)
+#library(ggplot2)
+library(dplyr)
+#library(gstat)
+#library(ncf) # for correlation 
+library(scales)
+
+##################################### Data Manipulation #########################################
+# 00 file
 
 ######################################### Read Data #############################################
 opar <- par()
 data <- read.csv('Spatial_Project/GSM_all_withDist.csv',header = T)
 data <- data[, -1]
+summary(data)
+#xtable(summary(data))
+
+######################################### Functions #############################################
+source('Spatial_Project/intermediate/01_PlotFunctions.R')
 
 ################################### Exploratory Data Analysis ###################################
-int.data <- interp(data$long, data$lat, data$freq)
-par(mfrow=c(1,1))
-image(int.data,xlab = 'longitude',ylab = 'latitude',xlim = range(D$long), ylim = range(D$lat))
-contour(int.data,add=TRUE)
-persp(int.data,xlab = 'longitude',ylab = 'latitude', zlab = 'temperature',theta = 60, phi = 30)
-
-gdata<-as.geodata(D,coords.col=1:2,data.col=3)
+image.combine(data)
+persp.combine(data)
 
 # descriptive stats
-summary(D$long)
-summary(D$lat)
-summary(D$freq)
-longitude<-obj[,1];latitude<-obj[,2];temp<-obj[,3]
-par(mfrow = c(2,2))
-hist(temp,xlab='temperature',main='Histogram of Temperature')
-boxplot(temp,xlab='temperature',main='Boxplot of Temperature')
-stem(temp)
-qqnorm(temp,xlab='Normal Quantiles')
-qqline(temp, lwd = 2)
-qqplot(runif(300,250,270),temp,xlab='Unifrom Quantiles',
-       ylab='Sample Quantiles',main='Unifrom Q-Q Plot')
-qqline(temp, distribution = function(p) qunif(p, 250,270),
-       prob = c(0.1, 0.6), lwd = 2)
-summary(temp)
-diff(range(temp))
+hist.combine(data)
+lgdata <- data
+lgdata$freq <- log(data$freq + 1)
+hist.combine(lgdata)
+boxplot.combine(data)
+boxplot.combine(lgdata)
+qq.combine(data)
+qq.combine(lgdata)
 
 ################################### Check spatial Trend ######################################### 
-plot(gdata)
+gdata5<-as.geodata(filter(data, hour == 5), coords.col=2:3,data.col=4)
+plot(gdata5)
+gdata6<-as.geodata(filter(data, hour == 6), coords.col=2:3,data.col=4)
+plot(gdata6)
+gdata7<-as.geodata(filter(data, hour == 7), coords.col=2:3,data.col=4)
+plot(gdata7)
+gdata8<-as.geodata(filter(data, hour == 8), coords.col=2:3,data.col=4)
+plot(gdata8)
+gdata9<-as.geodata(filter(data, hour == 9), coords.col=2:3,data.col=4)
+plot(gdata9)
 
 #points(gdata,xlab="Coord X",ylab="Coord Y")
-points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="rank.prop",main='rank')
+#points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="rank.prop",main='rank')
 #points(gdata,xlab="Coord X",ylab="Coord Y",cex.max=1.7,col=gray(seq(1,0.1,l=100)),pt.divide="equal")
-points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="quartile",main='quartile')
-points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="quintile",main='quintile')
-points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="decile",main='decile')
+#points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="quartile",main='quartile')
+#points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="quintile",main='quintile')
+#points(gdata,xlab="Coord X",ylab="Coord Y",pt.divide="decile",main='decile')
 
 # row & col boxplots
-par(mfrow=c(1,2))
-boxplot(matrix(obj[,3],nrow = length(unique(obj[,2])), ncol = length(unique(obj[,1]))),
-        xlab = 'x', main = 'Boxplot across x coord')
-boxplot(matrix(obj[,3],nrow = length(unique(obj[,1])), ncol = length(unique(obj[,2])),
-               byrow = T), xlab = 'y', main = 'Boxplot across y coord')
+long.combine(data)
+lat.combine(data)
 
 # regression
-fit1 <- lm(gdata$data ~ gdata$coords[,1] + gdata$coords[,2])
+fit1 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata5)
 summary(fit1)
-fit2 <- lm(gdata$data ~ gdata$coords[,1] + I(gdata$coords[,1]^2) + gdata$coords[,2]
-           + I(gdata$coords[,2]^2))
+stepAIC(fit1)
+fit2 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata6)
 summary(fit2)
-fit3 <- lm(gdata$data ~ gdata$coords[,1] + I(gdata$coords[,1]^2) + gdata$coords[,2]
-           + I(gdata$coords[,2]^2) + gdata$coords[,1]*gdata$coords[,2])
+stepAIC(fit2)
+fit3 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata7)
 summary(fit3)
-gres <- gdata
-gres$data <- residuals(fit3)
-plot(gres)
+stepAIC(fit3)
+fit4 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata8)
+summary(fit4)
+stepAIC(fit4)
+fit5 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata9)
+summary(fit5)
+stepAIC(fit5)
 
 # boxcox transformation
-par(mfrow=c(1,2))
-boxcox(fit2,lambda = seq(-10,0,0.1))
-boxcox(fit3,lambda = seq(-10,0,0.1))
+par(mfrow=c(2,3))
+boxcox(lm(data + 1 ~ .^2 + I(long^2) + I(lat^2), data = gdata5))
+boxcox(lm(data + 1 ~ .^2 + I(long^2) + I(lat^2), data = gdata6))
+boxcox(lm(data + 1 ~ .^2 + I(long^2) + I(lat^2), data = gdata7))
+boxcox(lm(data + 1 ~ .^2 + I(long^2) + I(lat^2), data = gdata8))
+boxcox(lm(data + 1 ~ .^2 + I(long^2) + I(lat^2), data = gdata9))
+par(mfrow=c(1,1))
 
-ldata <- log(gdata$data)
-lfit1 <- lm(ldata ~ gdata$coords[,1] + gdata$coords[,2])
+gdata5<-as.geodata(filter(lgdata, hour == 5), coords.col=2:3,data.col=4)
+plot(gdata5)
+gdata6<-as.geodata(filter(lgdata, hour == 6), coords.col=2:3,data.col=4)
+plot(gdata6)
+gdata7<-as.geodata(filter(lgdata, hour == 7), coords.col=2:3,data.col=4)
+plot(gdata7)
+gdata8<-as.geodata(filter(lgdata, hour == 8), coords.col=2:3,data.col=4)
+plot(gdata8)
+gdata9<-as.geodata(filter(lgdata, hour == 9), coords.col=2:3,data.col=4)
+plot(gdata9)
+# row & col boxplots
+long.combine(lgdata)
+lat.combine(lgdata)
+
+# regression
+lfit1 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata5)
 summary(lfit1)
-lfit2 <- lm(ldata ~ gdata$coords[,1] + I(gdata$coords[,1]^2) + gdata$coords[,2]
-            + I(gdata$coords[,2]^2))
+stepAIC(lfit1)
+lfit2 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata6)
 summary(lfit2)
-lfit3 <- lm(ldata ~ gdata$coords[,1] + I(gdata$coords[,1]^2) + gdata$coords[,2]
-            + I(gdata$coords[,2]^2) + gdata$coords[,1]*gdata$coords[,2])
+stepAIC(lfit2)
+lfit3 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata7)
 summary(lfit3)
-lgres <- gdata
-lgres$data <- residuals(lfit3)
-plot(lgres)
+stepAIC(lfit3)
+lfit4 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata8)
+summary(lfit4)
+stepAIC(lfit4)
+lfit5 <- lm(data ~ .^2 + I(long^2) + I(lat^2), data = gdata9)
+summary(lfit5)
+stepAIC(lfit5)
 
-sum <- rbind(c(summary(fit1)$adj.r.square, summary(fit2)$adj.r.square,
-               summary(fit3)$adj.r.square),c(summary(lfit1)$adj.r.square,
-                                             summary(lfit2)$adj.r.square, summary(lfit3)$adj.r.square))
-sum <- data.frame(sum)
-rownames(sum) <- c('Data','Log(Data)')
-colnames(sum) <- paste('Model',1:3,sep='')
-xtable(sum,digits = 4)
-xtable(summary(fit3))
+gres5 <- gdata5
+gres6 <- gdata6
+gres7 <- gdata7
+gres8 <- gdata8
+gres9 <- gdata9
+gres5$data <- fit1$residuals
+gres6$data <- fit2$residuals
+gres7$data <- fit3$residuals
+gres8$data <- fit4$residuals
+gres9$data <- fit5$residuals
+
+plot(gres5)
+plot(gres6)
+plot(gres7)
+plot(gres8)
+plot(gres9)
+
+gres5 <- data.frame(cbind(gres5$coords, gres5$data))
+gres5$hour <- 5
+names(gres5)[3] <- 'fre'
+gres6 <- data.frame(cbind(gres6$coords, gres6$data))
+gres6$hour <- 6
+names(gres6)[3] <- 'fre'
+gres7 <- data.frame(cbind(gres7$coords, gres7$data))
+gres7$hour <- 7
+names(gres7)[3] <- 'fre'
+gres8 <- data.frame(cbind(gres8$coords, gres8$data))
+gres8$hour <- 8
+names(gres8)[3] <- 'fre'
+gres9 <- data.frame(cbind(gres9$coords, gres9$data))
+gres9$hour <- 9
+names(gres9)[3] <- 'fre'
+gres <- bind_rows(gres5, gres6, gres7, gres8, gres9)
+
+#lgres5 <- gdata5
+#lgres6 <- gdata6
+#lgres7 <- gdata7
+#lgres8 <- gdata8
+#lgres9 <- gdata9
+#lgres5$data <- lfit1$residuals
+#lgres6$data <- lfit2$residuals
+#lgres7$data <- lfit3$residuals
+#lgres8$data <- lfit4$residuals
+#lgres9$data <- lfit5$residuals
+
+#plot(lgres5)
+#plot(lgres6)
+#plot(lgres7)
+#plot(lgres8)
+#plot(lgres9)
 
 # semivariogram
-par(mfrow=c(1,2))
-vario.class <- variog(gres,estimator.type="classical")
-vario.modul <- variog(gres,estimator.type="modulus")
-plot(vario.class,main='Variogram for Residual')
-points(vario.modul$u,vario.modul$v,col="red")
-legend('bottomright',legend = c('classic','Cressie'),,pch = 1,col = c(1,2))
-log.vario.class <- variog(lgres,estimator.type="classical")
-log.vario.modul <- variog(lgres,estimator.type="modulus")
-plot(log.vario.class,main='Variogram for log(residual)')
-points(log.vario.modul$u,log.vario.modul$v,col="red")
-legend('bottomright',legend = c('classic','Cressie'),,pch = 1,col = c(1,2))
-# correlogram
-c1 <- correlog(x = gres$coords[,1], y = gres$coords[,2], z = gres$data, increment = 5, 
-               resamp = 100, quiet = TRUE)
-plot(c1, main = 'Correlogram for Residuals')
-c2 <- correlog(x = gres$coords[,1], y = gres$coords[,2], z = lgres$data, increment = 5, 
-               resamp = 100, quiet = TRUE)
-plot(c2, main = 'Correlogram for Log(Residuals)')
+semivar.combine(gres)
+semivar.inone(gres, 0.4)
+semivar.inone(gres, 0.4/sqrt(2))
 
 #################################### Check Anistropy ###########################################
 # ESC
-source('esc.r')
-par(mfrow=c(1,1))
-ESC(gres$coords[,1],gres$coords[,2],gres$data)
+esc.combine(gres)
+
 # directional semivariogram
-par(opar)
-dir.vario <- variog4(gres)
-plot(dir.vario)
-# change coordinates
-coords <- gres$coords
-aniso.coords <- coords.aniso(coords,aniso.pars=c(0,3),reverse=FALSE)
-obj.aniso <- cbind(gres$data,aniso.coords)
-gres.aniso <- as.geodata(obj.aniso,coords.col=2:3,data.col=1)
-# check after transformation
-gres.var <- variog4(gres.aniso)
-par(mfrow=c(1,2))
-plot(gres.var)
-ESC(gres.aniso$coords[,1],gres.aniso$coords[,2],gres.aniso$data)
+dir.combine(gres)
 
 #################################### Model Fitting #############################################
 # empirical semivariogram
--- intermediate/01_PlotFunctions.R
+++ intermediate/01_PlotFunctions.R
@@ -12,7 +12,7 @@ library(dplyr)
 # This file writes functions for many types of plots of variable frequency for each hour 5:00 ~ 9:00
 
 ########################
-# plot functions
+# exploratory plot functions
 
 # 1. Image Plots
 image.combine <- function(D){
@@ -93,61 +93,115 @@ qq.combine <- function(D){
   par(mfcol=c(1,1))
 }
 
-# 2. semivarigram plot: 
-#    10 sub plots: 5 hour, 2 method: classical and cressie
-semivar.plot<-function(D)
-{
-  D$lgfreq<-log(D$freq+1)
-  uniq.hour<-unique(D$hour)
-  par(mfcol=c(2,length(uniq.hour)),mar=c(4,4,2,1))
+####################
+# spatial trend plot functions
+
+# 6. row & col boxplots
+long.combine <- function(D){
+  uniq.hour <- as.vector(distinct(D, hour)$hour)
+  par(mfrow = c(2, 3), mar = c(4, 4, 2, 1))
   for (i in uniq.hour)
   {
-    #simul.geo <- as.geodata(D[D$hour==i,c('long','lat','lgfreq')])
-    simul.geo <- as.geodata(D[D$hour==i,c('long','lat','freq')])
-    simul.var <- variog(simul.geo,estimator.type="classical",breaks=seq(0,0.4,0.01))
-    simul.modvar <- variog(simul.geo,estimator.type="modulus",breaks=seq(0,0.4,0.01))
-    plot(simul.var,pch=18,cex=0.8) 
-    plot(simul.modvar,pch=18,cex=0.8)
+    Di <- filter(D, hour == i)
+    boxplot(freq ~ long, data = Di, xlab = 'longitude', main = paste(i,': 00 ~',i+1,': 00 am'),
+            names = round(as.vector(arrange(distinct(Di, long), long)$long), digits = 2))
+  }
+  par(mfcol=c(1,1))
+}
+lat.combine <- function(D){
+  uniq.hour <- as.vector(distinct(D, hour)$hour)
+  par(mfrow = c(2, 3), mar = c(4, 4, 2, 1))
+  for (i in uniq.hour)
+  {
+    Di <- filter(D, hour == i)
+    boxplot(freq ~ lat, data = Di, xlab = 'latitude', main = paste(i,': 00 ~',i+1,': 00 am'),
+            names = round(as.vector(arrange(distinct(Di, lat), lat)$lat), digits = 2))
   }
   par(mfcol=c(1,1))
 }
 
-# 3. semivarigram plot: 
-#    1 plot: different colors for each hour; method: classical 
-semivar.plot2<-function(D,log.transf,xmax)
-  # log.transf==1: log transformation
-  #          ==0: original scale
+########################
+# semivariograms
+
+# 7. empirical semivarigram plots
+#    5 sub plots: 5 hour, 2 method: classical and cressie
+semivar.combine<-function(D)
 {
-  D$lgfreq<-log(D$freq+1)
-  uniq.hour<-unique(D$hour)
-  Var.tab<-data.frame()
+  uniq.hour <- as.vector(distinct(D, hour)$hour)
+  par(mfrow = c(2, 3), mar = c(4, 4, 2, 1))
   for (i in uniq.hour)
   {
-    if (log.transf==1)
-    {simul.geo <- as.geodata(D[D$hour==i,c('long','lat','lgfreq')])}
-    if (log.transf==0)
-    {simul.geo <- as.geodata(D[D$hour==i,c('long','lat','freq')])  }
-    simul.var <- variog(simul.geo,estimator.type="classical",breaks=seq(0,xmax,0.0005))
-    #simul.var <- variog(simul.geo,estimator.type="modulus",breaks=seq(0,0.4,0.01))
-    Var.tab<-rbind(Var.tab,simul.var$v)
+    Di <- filter(D, hour == i)
+    gDi <- as.geodata(Di, coords.col = 1:2, data.col = 3)
+    vario.class <- variog(gDi, estimator.type="classical", breaks=seq(0,0.4,0.01))
+    vario.modul <- variog(gDi, estimator.type="modulus", breaks=seq(0,0.4,0.01))
+    plot(vario.class, main = paste(i,': 00 ~',i+1,': 00 am'))
+    points(vario.modul$u, vario.modul$v, col="red")
+    legend('bottomright', legend = c('classic','Cressie'),pch = 1, cex = 0.8, col = c(1,2))
+  }
+  par(mfcol=c(1,1))
+}
+
+#    1 plots: different colors for each hour; method: Cressie weight 
+semivar.inone<-function(D, xmax)
+{
+  uniq.hour <- as.vector(distinct(D, hour)$hour)
+  Var.tab <- data.frame()
+  for (i in uniq.hour)
+  {
+    Di <- filter(D, hour == i)
+    gDi <- as.geodata(Di, coords.col = 1:2, data.col = 3)
+    vario.modul <- variog(gDi, estimator.type="modulus", breaks=seq(0,0.4,0.01))
+    Var.tab <- rbind(Var.tab, vario.modul$v)
   }
   # plot variance
   limx<-c(0,xmax)
   limy<-ceiling(max(Var.tab)*1.05)
   for (i in 1:length(uniq.hour))
   {
-    plot(simul.var$u,Var.tab[i,],pch=18,cex=0.7,col=i,xlim=limx,ylim=c(0,limy),yaxt='n',xaxt='n',xlab=' ',ylab=' ')
-    par(new=T)
+    plot(vario.modul$u, Var.tab[i,], pch = 18, cex = 0.7, col = i, xlim = limx, ylim = c(0,limy),
+         yaxt = 'n', xaxt = 'n', xlab = ' ', ylab = ' ')
+    par(new = T)
     print(i)
   }
-  axis(side=2,at=seq(0,limy,length.out=5),tcl=0.4,lwd.ticks=1,mgp=c(0,0.2,0))
+  axis(side = 2, at = seq(0, limy, length.out = 5), tcl = 0.4, lwd.ticks = 1, mgp = c(0,0.2,0))
   print(limy)
-  axis(side=1,at=seq(0,xmax,length.out=5),tcl=0.4,lwd.ticks=1,mgp=c(0,0.2,0))
-  mtext(side=1,text="Coordination Distance",line = 1.5)
-  mtext(side=2,text="Semivariance",line=1.5)
-  legend('topleft',bty='n',pch=rep(18,length(uniq.hour)), col=seq(1,length(uniq.hour),1),
-         legend = c('5:00-6:00','6:00-7:00','7:00-8:00','8:00-9:00','9:00-10:00'),cex=0.8)
-  par(new=F)
+  axis(side = 1, at = seq(0, xmax, length.out = 5), tcl = 0.4, lwd.ticks = 1, mgp = c(0,0.2,0))
+  mtext(side = 1, text = "Coordination Distance", line = 1.5)
+  mtext(side = 2, text = "Semivariance", line = 1.5)
+  legend('topleft', bty = 'n', pch = 18, col = 1:length(uniq.hour),
+         legend = c('5:00-6:00','6:00-7:00','7:00-8:00','8:00-9:00','9:00-10:00'), cex=0.8)
+  par(new = F)
+}
+
+###################
+# check anisotropy
+
+# 8. ESC plot
+esc.combine <- function(D){
+  uniq.hour <- as.vector(distinct(D, hour)$hour)
+  par(mfrow = c(2, 3), mar = c(4, 4, 2, 1))
+  for (i in uniq.hour)
+  {
+    Di <- filter(D, hour == i)
+    source('esc.r')
+    ESC(Di$long,Di$lat, Di$fre)
+  }
+  par(mfcol=c(1,1))
+}
+
+# 9. directional semivariogram
+dir.combine <- function(D){
+  uniq.hour <- as.vector(distinct(D, hour)$hour)
+  par(mfrow = c(2, 3), mar = c(4, 4, 2, 1))
+  for (i in uniq.hour)
+  {
+    Di <- filter(D, hour == i)
+    gDi <- as.geodata(Di, coords.col = 1:2, data.col = 3)
+    dir.vario <- variog4(gDi)
+    plot(dir.vario, xlab = paste(i,': 00 ~',i+1,': 00 am'))
+  }
+  par(mfcol=c(1,1))
 }
 
 # 4. LSE fit lines
@@ -197,103 +251,4 @@ LSE.fit<-function(D,Model,log.transf,xmax)
   return(paratab)
 }
 
-
-
-
-Corr<-function(D,StepLen,xmax)
-  # plot correlation
-  # D: dataset;  
-  # D$freq.k in 1000 scale
-  # output: 6th row: mean of each bin
-{
-  D$freq.k<-D$freq/1000
-  uniq.hour<-unique(D$hour)
-  limx<-c(0,xmax)
-  corrtab<-data.frame()
-  for (i in uniq.hour)
-  {
-    Dsub<-D[D$hour==i,]
-    print(i)
-    ncf.cor <- correlog(Dsub$long, Dsub$lat,Dsub$freq.k, increment=0.01,resamp = 100)
-    plot(ncf.cor$mean.of.class,ncf.cor$correlation,type='l',
-         pch=18,cex=0.7,col=i-4,xlim=limx,ylim=c(-0.3,1),yaxt='n',xaxt='n',xlab='',ylab='')
-    par(new=T)
-    corrtab<-rbind(corrtab,ncf.cor$correlation)
-  }
-  corrtab<-rbind(corrtab,ncf.cor$mean.of.class)
-  axis(side=2,at=round(seq(-0.2,1,length.out=7),1),tcl=0.4,lwd.ticks=1,mgp=c(0,0.2,0))
-  axis(side=1,at=round(seq(0,xmax,length.out=5),2),tcl=0.4,lwd.ticks=1,mgp=c(0,0.2,0))
-  mtext(side=1,text="Coordination Distance",line = 1.5)
-  mtext(side=2,text="Correlation",line=1.5)
-  mtext(side=3,text=" ",line=0.5)
-  legend(x=0.085,y=0.9,bty='n',pch=rep(18,5),lty=rep('solid',5), col=1:5,
-         legend = c('5:00-6:00','6:00-7:00','7:00-8:00','8:00-9:00','9:00-10:00'),cex=1)
-  par(new=F) 
-  return(corrtab)
-    
-}
-
-
-Cortab<-Corr(D,StepLen=0.01,xmax=0.15)
-write.csv(Cortab,'Corrtab.csv')
-
-Cortab<-read.csv('Corrtab.csv')
-
-xmax=0.15
-ind<-Cortab[6,]<=xmax
-
-limx=c(0,xmax)
-
-for( i in 1:5)
-{
-  x<-as.numeric(Cortab[6,ind])
-  y<-as.numeric(Cortab[i,ind])
-  plot(x,y,type = 'l',pch=18,cex=0.7,col=i,xlim=limx,ylim=c(-0.3,1),yaxt='n',xaxt='n',xlab=' ',ylab=' ')
-  par(new=T)
-}
-axis(side=2,at=round(seq(-0.2,1,length.out=7),1),tcl=0.4,lwd.ticks=1,mgp=c(0,0.2,0))
-axis(side=1,at=round(seq(0,xmax,length.out=5),2),tcl=0.4,lwd.ticks=1,mgp=c(0,0.2,0))
-mtext(side=1,text="Coordination Distance",line = 1.5)
-mtext(side=2,text="Correlation",line=1.5)
-mtext(side=3,text=" ",line=0.5)
-legend(x=0.09,y=1,bty='n',lty=rep('solid',5), col=1:5,
-       legend = c('5:00-6:00','6:00-7:00','7:00-8:00','8:00-9:00','9:00-10:00'),cex=0.9,y.intersp = 0.7)
-par(new=F) 
-
-
-##################
-# Plot
-hist.plot(D)
-
-semivar.plot(D)
-
-semivar.plot2(D,1,0.4) # log
-semivar.plot2(D,0,xmax = 0.15)
-
-#log scale
-LSE.fit(D,'gaussian',1,c(10,0.3))  # log
-LSE.fit(D,'matern',1,c(20,0.3))
-LSE.fit(D,'exponential',1,c(10,0.3))
-
-#orginal 
-LSE.fit(D,'gaussian',0,xmax=0.4)  # orginal 
-Gau<-LSE.fit(D,'gaussian',0,xmax=0.15)  # orginal
-Cub<-LSE.fit(D,'cubic',0,xmax=0.15) 
-Sph<-LSE.fit(D,'spherical',0,xmax=0.15) 
-Mat<-LSE.fit(D,'matern',0,xmax=0.15) 
-Exp<-LSE.fit(D,'exponential',0,xmax=0.15) 
-
-#############
-D$lgfreq<-log(D$freq+1)
-
-  simul.geo <- as.geodata(D[,c('long','lat','lgfreq')])
-  #simul.geo <- as.geodata(D[,c('long','lat','freq')])
-  
-  simul.var <- variog(simul.geo,estimator.type="classical",breaks=seq(0,0.4,0.0005))
-  #simul.modvar <- variog(simul.geo,estimator.type="modulus",breaks=seq(0,0.4,0.0005))
-  plot(simul.var,pch=18,cex=0.8) 
-  #plot(simul.modvar,pch=18,cex=0.8)
-
-
-#############
 
